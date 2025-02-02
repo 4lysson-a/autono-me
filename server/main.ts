60305603d66f3ef9fe80fc93a06990eca2d47f79
Alysson Alcântara <4ly.alcantara@gmail.com>
2025-02-01 22:02:44 -0300

    feat: add initial server setup with Prisma and Express controllers (#3)

--- server/main.ts
+++ server/main.ts
@@ -1,31 +1,38 @@
-import { Application } from "jsr:@oak/oak/application";
-import { Router } from "jsr:@oak/oak/router";
+import express from "express";
+import cors from "cors";
 
-const data = [
-  { name: "Tyrannosaurus Rex", diet: "Carnivore" },
-  { name: "Velociraptor", diet: "Carnivore" },
-];
+import {
+  handleCreateTransaction,
+  handleDeleteTransaction,
+  handleGetTransactions,
+  handleGetTransactionsById,
+  handleUpdateTransaction,
+} from "./controllers/transaction.js";
+import { handleCreateTestUser } from "./controllers/user.js";
 
-export const app = new Application();
-const router = new Router();
+const app = express();
+const port = 8000;
 
-router.get("/api/dinosaurs", (context) => {
-  context.response.body = data;
+// Configuração do CORS para permitir apenas requisições da porta 5432
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+// Middleware para parsing de JSON
+app.use(express.json());
+
+// Definição das rotas
+app.get("/transactions", handleGetTransactions);
+app.get("/transactions/:id", handleGetTransactionsById);
+app.post("/transactions", handleCreateTransaction);
+app.put("/transactions/:id", handleUpdateTransaction);
+app.delete("/transactions/:id", handleDeleteTransaction);
+
+app.post("/users", handleCreateTestUser);
+
+// Inicializa o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
-router.get("/api/dinosaurs/:dinosaur", (context) => {
-  if (!context?.params?.dinosaur) {
-    context.response.body = "No dinosaur name provided.";
-  }
-
-  const dinosaur = data.find(
-    (item) => item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-  );
-
-  context.response.body = dinosaur ?? "No dinosaur found.";
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-await app.listen({ port: 8000 });
