04edb8b8193b82355f1aaeae93726322e96c27bc
4lysson-a <4ly.alcantara@gmail.com>
2025-02-03 21:34:53 -0300

    feat: add transaction type to recent transactions and update component styling

--- client/src/pages/(private)/dash/sections/recent-transactions.tsx
+++ client/src/pages/(private)/dash/sections/recent-transactions.tsx
@@ -1,17 +1,24 @@
-import { ArrowDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ArrowDown as Arrow } from "lucide-react";
 
-type Transaction = {
+type ITransaction = {
   date: string;
   title: string;
   amount: string;
+  type: "income" | "expense";
 };
 
-const Transaction: React.FC<Transaction> = ({ date, title, amount }) => {
+const Transaction: React.FC<ITransaction> = ({ date, title, amount, type }) => {
   return (
     <li className="flex flex-row justify-between items-center">
       <div className="flex items-center gap-3 xs:gap-4">
         <div className="bg-ui-border rounded-lg p-2 xs:p-3">
-          <ArrowDown className="w-5 h-5 xs:w-auto xs:h-auto" />
+          <Arrow
+            className={cn(
+              "w-5 h-5 xs:w-auto xs:h-auto",
+              type == "expense" && "rotate-180"
+            )}
+          />
         </div>
         <div>
           <p className="text-sm xs:text-md">{title}</p>
@@ -26,26 +33,30 @@ const Transaction: React.FC<Transaction> = ({ date, title, amount }) => {
   );
 };
 
-const mockData = [
+const mockData: ITransaction[] = [
   {
     date: "Today",
     title: "Payment from John Doe",
     amount: "$1,000.00",
+    type: "income",
   },
   {
     date: "Yesterday",
     title: "Payment from Jane Doe",
     amount: "$1,000.00",
+    type: "expense",
   },
   {
-    date: "Yesterday",
+    date: "Jan 2",
     title: "Payment from Jane Doe",
     amount: "$1,000.00",
+    type: "income",
   },
   {
-    date: "Yesterday",
+    date: "Jan 1",
     title: "Payment from Jane Doe",
     amount: "$1,000.00",
+    type: "expense",
   },
 ];
 
@@ -58,6 +69,7 @@ export const RecentTransactions = () => {
         {mockData.map((data) => (
           <Transaction
             date={data.date}
+            type={data.type}
             key={data.title}
             title={data.title}
             amount={data.amount}
