525139b47a675404a943e386628f19acff395899
Alysson Alcântara <4ly.alcantara@gmail.com>
2025-03-01 15:31:58 -0300

    feat: personalize greeting in home component with user name

--- client/src/pages/(private)/dash/home.tsx
+++ client/src/pages/(private)/dash/home.tsx
@@ -1,12 +1,17 @@
 import { Search } from "lucide-react";
 import { RecentTransactions } from "./sections/recent-transactions";
 import { FinancialInsights } from "./sections/financial-insights";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Home = () => {
+  const { user } = useAuth0();
+
   return (
     <div className="pb-[50px]">
       <header className="flex p-4 py-6">
-        <h1 className="text-center w-full font-bold text-lg">Olá, João</h1>
+        <h1 className="text-center w-full font-bold text-lg">
+          Olá, {user?.name?.split(" ")[0] ?? "Guest"} {" :)"}
+        </h1>
         <Search className="place-items-end" />
       </header>
 
